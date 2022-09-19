f0ff112dbaed8baefaa22005ae924c1267bccf44
stuardo asus-tuf <stuardo.lucho@pucp.edu.pe>
2022-09-19 11:27:24 -0500

    desarrollo en clase 6

--- src/main/java/com/example/clase6gtics/controller/ProductController.java
+++ src/main/java/com/example/clase6gtics/controller/ProductController.java
@@ -7,12 +7,13 @@ import com.example.clase6gtics.repository.SupplierRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.beans.PropertyEditorSupport;
 import java.util.Optional;
 
 @Controller
@@ -34,33 +35,43 @@ public class ProductController {
     }
 
     @GetMapping("/new")
-    public String nuevoProductoFrm(Model model) {
+    public String nuevoProductoFrm(@ModelAttribute("product") Product product,
+                                   Model model) {
         model.addAttribute("listaCategorias", categoryRepository.findAll());
-        model.addAttribute("listaProveedores",supplierRepository.findAll());
-        return "product/newFrm";
+        model.addAttribute("listaProveedores", supplierRepository.findAll());
+        return "product/editFrm";
     }
 
     @PostMapping("/save")
-    public String guardarProducto(Product product, RedirectAttributes attr) {
-        if (product.getId() == 0) {
-            attr.addFlashAttribute("msg", "Producto creado exitosamente");
+    public String guardarProducto(Model model, RedirectAttributes attr,
+                                  @ModelAttribute("product") @Valid Product product,
+                                  BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("listaCategorias", categoryRepository.findAll());
+            model.addAttribute("listaProveedores", supplierRepository.findAll());
+            return "product/editFrm";
         } else {
-            attr.addFlashAttribute("msg", "Producto actualizado exitosamente");
+            if (product.getId() == 0) {
+                attr.addFlashAttribute("msg", "Producto creado exitosamente");
+            } else {
+                attr.addFlashAttribute("msg", "Producto actualizado exitosamente");
+            }
+            productRepository.save(product);
+            return "redirect:/product";
         }
-        productRepository.save(product);
-        return "redirect:/product";
     }
 
     @GetMapping("/edit")
-    public String editarTransportista(Model model,@RequestParam("id") int id) {
+    public String editarTransportista(@ModelAttribute("product") Product product,
+                                      Model model, @RequestParam("id") int id) {
 
         Optional<Product> optProduct = productRepository.findById(id);
 
         if (optProduct.isPresent()) {
-            Product product = optProduct.get();
+            product = optProduct.get();
             model.addAttribute("product", product);
             model.addAttribute("listaCategorias", categoryRepository.findAll());
-            model.addAttribute("listaProveedores",supplierRepository.findAll());
+            model.addAttribute("listaProveedores", supplierRepository.findAll());
             return "product/editFrm";
         } else {
             return "redirect:/product";
@@ -76,10 +87,27 @@ public class ProductController {
 
         if (optProduct.isPresent()) {
             productRepository.deleteById(id);
-            attr.addFlashAttribute("msg","Producto borrado exitosamente");
+            attr.addFlashAttribute("msg", "Producto borrado exitosamente");
         }
         return "redirect:/product";
 
+    }
+
+    @InitBinder("product")
+    public void validadorProducto(WebDataBinder binder) {
+        PropertyEditorSupport integerValidator = new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException {
+                try {
+                    setValue(Integer.parseInt(text));
+                } catch (NumberFormatException e) {
+                    setValue(0);
+                }
+            }
+        };
+
+        binder.registerCustomEditor(Integer.class, "unitsonorder", integerValidator);
+        binder.registerCustomEditor(Integer.class,"reorderlevel",integerValidator);
     }
 
 }
--- src/main/java/com/example/clase6gtics/entity/Product.java
+++ src/main/java/com/example/clase6gtics/entity/Product.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 
 @Entity
@@ -16,8 +17,12 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "productid")
     private int id;
+
     @Column(nullable = false)
+    @Size(max = 40, message = "El tamaño máximo es 40 caracteres")
+    @NotBlank
     private String productname;
+
     @ManyToOne
     @JoinColumn(name = "supplierid")
     private Supplier supplier;
@@ -25,9 +30,21 @@ public class Product {
     @JoinColumn(name = "categoryid")
     private Category category;
     private String quantityperunit;
+
+    @Digits(integer = 10, fraction = 4)
+    @Positive
     private BigDecimal unitprice;
+
+    @Digits(integer = 10,fraction = 0)
+    @Max(value = 32767)
+    @Min(value = 0)
     private int unitsinstock;
+
+    @Digits(integer = 10,fraction = 0)
+    @Max(value = 32767)
+    @Positive(message = "Tiene que ser mayor a 0")
     private int unitsonorder;
+
     private int reorderlevel;
     @Column(nullable = false)
     private boolean discontinued;
